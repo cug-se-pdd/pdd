57ae03fdd7dcff5d8adfe8b562b097fc4dc0b192
959AI994 <115469462+959AI994@users.noreply.github.com>
2024-04-19 13:36:06 +0800

    '2'

---
+++ uniapp小程序端（初版-页面不兼容）/miniprogram/pages/webview/webview.js
@@ -0,0 +1,13 @@
+// pages/webview/webview.js
+Page({
+  data: {
+    // 页面的初始数据
+  },
+  onLoad: function(options) {
+    // 页面加载时执行
+  },
+  onShow: function() {
+    // 页面显示时执行
+  }
+  // 这里可以添加更多的事件处理函数
+});
