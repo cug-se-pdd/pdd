8dde36738bdc2822d212816f9dbde1df1bc512dc
Wyx <a531806879@163.com>
2024-05-07 18:41:48 +0800

    万贻祥

--- CinemaBookingSystem/panda-user/src/views/cinema/ChooseSession.vue
+++ CinemaBookingSystem/panda-user/src/views/cinema/ChooseSession.vue
@@ -18,7 +18,7 @@
             <div class="phone-number">电话: {{cinemaInfo.cinemaPhone}}</div>
             <div style="margin: 25px 0;">活动地点简介 ————————————————————</div>
 <!--            <div class="service"><span class="service-tag">免费停车</span>观影前1小时至观影后1小时内免费停车</div>-->
-<!--            <div class="service"><span class="service-tag">领快乐水</span>节假日期间，每张票可领取一瓶快乐水</div>-->
+<!--            <div class="service"><span class="service-tag">领快乐水</span>节假日期间，每张票可领取一瓶快乐水 </div>-->
             <div class="service"><span class="service-tag">211高校</span></div>
             <div class="service"><span class="service-tag">环境优美</span></div>
           </div>
